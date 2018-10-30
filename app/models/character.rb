851bc2b42506417a41b0af8c4dfa859f16ae7fe4
Peter-G-Stone <peter.stone.online@gmail.com>
2018-10-30 19:16:15 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,18 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+#   def build_show(name)
+
+#     self.show = Show.find_or_create_by(name)
+#   end
+
+#   def build_network(net)
+#     self.show.network = Network.find_or_create_by(net)
+#   end
+
 end
